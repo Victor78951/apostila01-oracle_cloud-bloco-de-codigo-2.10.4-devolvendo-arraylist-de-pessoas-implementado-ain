82c76696fddbbf4583f33f3c087b64381e4cd411
unknown <aluno@L1M03.IPIRANGA.FATEC>
2022-11-03 18:22:07 -0300

    apostila01 (oracle_cloud): bloco de codigo 2.10.4, devolvendo arraylist de pessoas implementado; ainda não temos o menu principal

---
+++ src/PessoaService.java
@@ -0,0 +1,53 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.util.*;
+
+
+import org.json.*;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RequiredArgsConstructor
+public class PessoaService {
+    private static HttpClient client = HttpClient.newHttpClient();
+    private final String url;
+
+    public List <Pessoa> listar() {
+        //Date e SimpleFormat
+        //generics (desde a versão 5)
+        List <Pessoa> pessoas = new ArrayList <> (); //diamante: versão 7
+        try {
+            // design pattern: builder
+            HttpRequest request = HttpRequest.newBuilder()
+
+                .uri(URI.create(url))
+                .build();
+            var response = client.send(request, BodyHandlers.ofString());
+            //System.out.println(response.body());
+            JSONObject raiz = new JSONObject (response.body());
+            JSONArray items = raiz.getJSONArray("items");
+            for (int i = 0; i < items.length(); i++) {
+            JSONObject pessoaJSON = items.getJSONObject(i);
+            String nome = pessoaJSON.getString("nome");
+            
+            int idade = pessoaJSON.getInt("idade");
+            String hobby = pessoaJSON.getString("hobby");
+            //System.out.printf("%s %d %s\n", nome, idade, hobby);
+            Pessoa p = new Pessoa();
+            p.setNome(nome);
+            p.setIdade(idade);
+            p.setHobby(hobby);
+            pessoas.add(p);
+            }
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return pessoas;
+    }
+}
